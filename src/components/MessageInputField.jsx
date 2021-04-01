9afe3b73849cb03bf8f45018e1861706fc916edb
Yutan0423 <yuutan3240@gmail.com>
2021-04-01 13:31:15 +0900

    qqqqqqqqqq

---
+++ src/components/MessageInputField.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    root: {
+        gridRow: 2
+    }
+});
+
+const MessageInputField = () => {
+    const classes = useStyles();
+
+    return (
+        <div className={classes}>Message Input Field</div>
+    );
+}
+
+export default MessageInputField;
