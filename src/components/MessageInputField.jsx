72daec34223d0048f1880ddc42243070050cff9e
Yutan0423 <yuutan3240@gmail.com>
2021-04-01 17:40:45 +0900

    aaaaaaaaaaa

--- src/components/MessageInputField.jsx
+++ src/components/MessageInputField.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { Avatar, Grid } from '@material-ui/core';
+import { gravatarPath } from '../gravatar';
 
 const useStyles = makeStyles({
     root: {
-        gridRow: 2
+        gridRow: 2,
+        margin: '26px'
     }
 });
 
-const MessageInputField = () => {
+const MessageInputField = ({name}) => {
     const classes = useStyles();
+    const avatarPath = gravatarPath(name);
 
     return (
-        <div className={classes}>Message Input Field</div>
+        <div className={classes}>
+            <Grid container>
+                <Grid xs={1}>
+                    <Avatar src={avatarPath} />
+                </Grid>
+                <Grid xs={10}>入力</Grid>
+                <Grid xs={1}>ボタン</Grid>
+            </Grid>
+        </div>
     );
 }
 
