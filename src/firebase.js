3afba7e54064cb806550df1a9fa9f1220012cb66
Yutan0423 <yuutan3240@gmail.com>
2021-04-02 10:48:54 +0900

    aaaaaaaaaaaa

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,30 @@
 import firebase from 'firebase';
 
+console.log(process.env.REACT_APP_FOO);
+
+const {
+    REACT_APP_FIREBASE_API_KEY,
+    REACT_APP_FIREBASE_AUTH_DOMAIN,
+    REACT_APP_FIREBASE_PROJECT_ID,
+    REACT_APP_FIREBASE_STORAGE_BUCKET,
+    REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    REACT_APP_FIREBASE_APP_ID
+} = process.env;
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAQkXvIFLuyb9_cRFAWKaOIvX4lq-ErbAw",
-    authDomain: "react-chat-app-58761.firebaseapp.com",
-    projectId: "react-chat-app-58761",
-    storageBucket: "react-chat-app-58761.appspot.com",
-    messagingSenderId: "350586560705",
-    appId: "1:350586560705:web:ac147ea88ddd6c17797440"
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
-const messageRef = database.ref('messages');
+const messagesRef = database.ref('messages');
  
 export const pushMessage = ({ name, text }) => {
-    messageRef.push({ name, text })
+    messagesRef.push({ name, text })
     console.log("saaaaaaa");
 }
