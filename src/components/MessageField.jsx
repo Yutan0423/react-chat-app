34c5639ec4a86457bc1d70b95f0281fc1b20413b
Yutan0423 <yuutan3240@gmail.com>
2021-04-02 16:24:49 +0900

    aaaaaaaaaaa

--- src/components/MessageField.jsx
+++ src/components/MessageField.jsx
@@ -17,7 +17,7 @@ const MessageField = ({inputEl, name, setText, text}) => {
                 const text = e.target.value;
                 if(text === '') return;
                 if(e.key === 'Enter') {
-                    pushMessage({name: '丹さん', text});
+                    pushMessage({name, text});
                     setText('');
                     e.preventDefault();
                 }
